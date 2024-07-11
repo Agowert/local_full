f75d98d1c1b1bbf5d026b6d006ffbba77d38d1a1
Agowert <70849596+Agowert@users.noreply.github.com>
2024-07-11 10:06:12 +0300

    ex1-42
     Верхнее меню сайта: поддержка стиля отображения пункта меню.
    Решаемая задача
     Верхнее меню должно поддерживать управление цветом текста пунктов.
     Администратор сайта, без редактирования исходного кода, должен иметь возможность
    установить цвет пункта меню, указав имя класса стиля.
     Решение должно учитывать, что в будущем будут добавляться новые классы стиля в css файл,
    например
    .nav .menu .color-yellow{
     color: #eeff0e;
    }
    которые должны поддерживаться без переделки шаблона компонента.
     Пример меню с разным цветом пунктов – в файле верстки main_color_menu.html.
     Для демонстрации решения задать красный цвет для пункта меню «Компания»).

--- local/templates/exam1/components/bitrix/menu/horizontal_exam/template.php
+++ local/templates/exam1/components/bitrix/menu/horizontal_exam/template.php
@@ -19,11 +19,23 @@ foreach($arResult as $arItem):?>
 	<?if ($previousLevel && $arItem["DEPTH_LEVEL"] < $previousLevel):?>
 		<?=str_repeat("</ul></li>", ($previousLevel - $arItem["DEPTH_LEVEL"]));?>
 	<?endif?>
+	
+	<? 
+			
+			$Col = "";
+			if(isset($arItem['PARAMS']['COLOR'])) { 
+			$Col = $arItem['PARAMS']['COLOR']?>
 
+<?}?>
 	<?if ($arItem["IS_PARENT"]):?>
-
+	
 		<?if ($arItem["DEPTH_LEVEL"] == 1):?>
-			<li><a href="<?=$arItem["LINK"]?>" class="<?if ($arItem["SELECTED"]):?>root-item-selected<?else:?>root-item<?endif?>"><?=$arItem["TEXT"]?></a>
+
+
+
+
+
+			<li><a href="<?=$arItem["LINK"]?>" class="<?if ($arItem["SELECTED"]):?>root-item-selected<?else:?>root-item<?endif?> <?=$Col?> " ><?=$arItem["TEXT"]?></a>
 				<ul>
 		<?else:?>
 			<li<?if ($arItem["SELECTED"]):?> class="item-selected"<?endif?>><a href="<?=$arItem["LINK"]?>" class="parent"><?=$arItem["TEXT"]?></a>
@@ -32,7 +44,9 @@ foreach($arResult as $arItem):?>
 		<? 
 		
 		?>
-		<? if(isset($arItem['PARAMS']['main_text_menu'])) { ?>
+		
+		<?
+		if(isset($arItem['PARAMS']['main_text_menu'])) { ?>
 
 			<div class="menu-text"><?=$arItem['PARAMS']['main_text_menu'] ?></div>
 
@@ -44,7 +58,7 @@ foreach($arResult as $arItem):?>
 		<?if ($arItem["PERMISSION"] > "D"):?>
 
 			<?if ($arItem["DEPTH_LEVEL"] == 1):?>
-				<li><a href="<?=$arItem["LINK"]?>" class="<?if ($arItem["SELECTED"]):?>root-item-selected<?else:?>root-item<?endif?>"><?=$arItem["TEXT"]?></a></li>
+				<li><a href="<?=$arItem["LINK"]?>" class="<?if ($arItem["SELECTED"]):?>root-item-selected<?else:?>root-item<?endif?> <?=$Col?>"><?=$arItem["TEXT"]?></a></li>
 			<?else:?>
 				<li<?if ($arItem["SELECTED"]):?> class="item-selected"<?endif?>><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
 			<?endif?>
